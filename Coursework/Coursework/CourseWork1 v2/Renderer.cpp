d0e26f9249625c90f98d856d707e68a75527c866
nutiel <32769684+nutiel@users.noreply.github.com>
2017-11-23 21:06:09 +0000

    done2
    done2

--- Coursework/Coursework/CourseWork1 v2/Renderer.cpp
+++ Coursework/Coursework/CourseWork1 v2/Renderer.cpp
@@ -25,10 +25,6 @@ Renderer::Renderer(Window & parent) : OGLRenderer(parent) {
 	brickBump = SOIL_load_OGL_texture(TEXTUREDIR"brickDOT3.tga", SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, 0);
 	SetTextureRepeating(brickBump, true);
 
-	//Bush texture used from https://www.google.co.uk/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwijwqu5v9XXAhWKHsAKHTqzAIoQjRwIBw&url=https%3A%2F%2Ffreestocktextures.com%2Ftexture%2Fnature-bush-green%2C688.html&psig=AOvVaw1wMxeE3p6Weccs4VBBVATk&ust=1511553962355798
-	bushTexture = SOIL_load_OGL_texture(TEXTUREDIR"bush.jpg", SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, 0);
-	SetTextureRepeating(bushTexture, true);
-
 	//---Shaders---//
 
 	floorShader = new Shader(SHADERDIR"BumpVertex.glsl", SHADERDIR"BumpFragment.glsl");
@@ -109,18 +105,6 @@ Renderer::Renderer(Window & parent) : OGLRenderer(parent) {
 	root3->AddChild(new Cube(camera->GetPosition()));
 	root3->getChildren()[0]->setShader(eyeShader);
 
-	hellData = new MD5FileData(MESHDIR"hellknight.md5mesh");
-	hellNode = new MD5Node(*hellData);
-	
-	hellData -> AddAnim(MESHDIR"idle2.md5anim");
-	hellNode -> PlayAnim(MESHDIR"idle2.md5anim");
-	
-	sceneShader = new Shader(SHADERDIR"shadowscenevert.glsl", SHADERDIR"shadowscenefrag.glsl");
-	shadowShader = new Shader(SHADERDIR"shadowVert.glsl", SHADERDIR"shadowFrag.glsl");
-	if (!sceneShader -> LinkProgram() || !shadowShader -> LinkProgram()) {
-		return;
-	}
-
 	counter = 0.0f;
 	frames = 0;
 	fps = 0;
@@ -133,26 +117,6 @@ Renderer::Renderer(Window & parent) : OGLRenderer(parent) {
 	//A new particle emitter!
 	emitter = new ParticleEmitter();
 
-	glGenTextures(1, &shadowTex);
-	glBindTexture(GL_TEXTURE_2D, shadowTex);
-	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, SHADOWSIZE, SHADOWSIZE, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
-	
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_COMPARE_MODE, GL_COMPARE_R_TO_TEXTURE);
-	
-	glBindTexture(GL_TEXTURE_2D, 0);
-	glGenFramebuffers(1, &shadowFBO);
-	glBindFramebuffer(GL_FRAMEBUFFER, shadowFBO);
-	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, shadowTex, 0);
-	glDrawBuffer(GL_NONE);
-	glBindFramebuffer(GL_FRAMEBUFFER, 0);
-
-	floor = Mesh::GenerateQuad();
-	floor -> SetTexture(SOIL_load_OGL_texture(TEXTUREDIR"brick.tga", SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_MIPMAPS));
-	floor -> SetBumpMap(SOIL_load_OGL_texture(TEXTUREDIR"brickDOT3.tga", SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_MIPMAPS));
 	glEnable(GL_TEXTURE_CUBE_MAP_SEAMLESS);
 	init = true;
 
@@ -179,10 +143,9 @@ Renderer ::~Renderer(void) {
 	delete root2;
 	delete root3;
 	delete quad;
-	delete emitter;	delete hellData;
-	delete hellNode;
-	delete floor;	delete sceneShader;
-	delete shadowShader;
+	delete emitter;
+
+
 	currentShader = NULL;
 	Cube::DeleteCube();
 	Sphere::DeleteSphere();
@@ -333,10 +296,6 @@ void Renderer::RenderScene() {
 		showFPS();
 		break;
 	case 2:
-		/*glEnable(GL_DEPTH_TEST);
-		DrawShadowScene(); // First render pass ...
-		DrawCombinedScene(); // Second render pass ...
-		glDisable(GL_DEPTH_TEST);*/
 		BuildNodeLists(root3);
 		SortNodeLists();
 
@@ -351,75 +310,7 @@ void Renderer::RenderScene() {
 	SwapBuffers();
 }
 
-void Renderer::DrawShadowScene() {
-	glBindFramebuffer(GL_FRAMEBUFFER, shadowFBO);
-	
-	glClear(GL_DEPTH_BUFFER_BIT);
-	
-	glViewport(0, 0, SHADOWSIZE, SHADOWSIZE);
-	
-	glColorMask(GL_FALSE, GL_FALSE, GL_FALSE, GL_FALSE);
-	
-	SetCurrentShader(shadowShader);
-	Matrix4 tview = viewMatrix;
-	viewMatrix = Matrix4::BuildViewMatrix(
-	light -> GetPosition(), Vector3(0, 0, 0));
-	textureMatrix = biasMatrix *(projMatrix * viewMatrix);
-	
-	UpdateShaderMatrices();
-	
-	DrawFloor();
-	DrawMesh();
-	
-	glUseProgram(0);
-	glColorMask(GL_TRUE, GL_TRUE, GL_TRUE, GL_TRUE);
-	glViewport(0, 0, width, height);
-	
-	glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
-	viewMatrix = tview;
-}
-
-void Renderer::DrawCombinedScene() {
-	SetCurrentShader(sceneShader);
-	glUniform1i(glGetUniformLocation(currentShader -> GetProgram(), "diffuseTex"), 0);
-	glUniform1i(glGetUniformLocation(currentShader -> GetProgram(), "bumpTex"), 1);
-	glUniform1i(glGetUniformLocation(currentShader -> GetProgram(), "shadowTex"), 2);
-	
-	glUniform3fv(glGetUniformLocation(currentShader -> GetProgram(), "cameraPos"), 1, (float *)& camera -> GetPosition());
-	
-	SetShaderLight(*light);
-	
-	glActiveTexture(GL_TEXTURE2);
-	glBindTexture(GL_TEXTURE_2D, shadowTex);
-	Matrix4 tview = viewMatrix;
-	viewMatrix = camera -> BuildViewMatrix();
-	UpdateShaderMatrices();
-	
-	DrawFloor();
-	DrawMesh();
-	
-	glUseProgram(0);	viewMatrix = tview;}void Renderer::DrawMesh() {
-	modelMatrix.ToIdentity();
-	
-	Matrix4 tempMatrix = textureMatrix * modelMatrix;
-	
-	glUniformMatrix4fv(glGetUniformLocation(currentShader -> GetProgram(), "textureMatrix"), 1, false, *& tempMatrix.values);
-	
-	glUniformMatrix4fv(glGetUniformLocation(currentShader -> GetProgram(), "modelMatrix"), 1, false, *& modelMatrix.values);
-	
-	hellNode -> Draw(*this);
-	
-}
-
-void Renderer::DrawFloor() {
-	modelMatrix = Matrix4::Rotation(90, Vector3(1, 0, 0)) * Matrix4::Scale(Vector3(450, 450, 1));
-	Matrix4 tempMatrix = textureMatrix * modelMatrix;
-	
-	glUniformMatrix4fv(glGetUniformLocation(currentShader -> GetProgram(), "textureMatrix"), 1, false, *& tempMatrix.values);
-	glUniformMatrix4fv(glGetUniformLocation(currentShader -> GetProgram(), "modelMatrix"), 1, false, *& modelMatrix.values);
-	
-	floor -> Draw();}
 void Renderer::showFPS() {
 	SetCurrentShader(textShader);
 	glUniform1i(glGetUniformLocation(textShader->GetProgram(), "diffuseTex"), 0);
@@ -658,7 +549,9 @@ void Renderer::DrawParticles() {
 
 void Renderer::SetShaderParticleSize(float f) {
 	glUniform1f(glGetUniformLocation(currentShader->GetProgram(), "particleSize"), f);
-}void Renderer::DrawWater() {
+}
+
+void Renderer::DrawWater() {
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
@@ -695,7 +588,13 @@ void Renderer::SetShaderParticleSize(float f) {
 	
 	quad -> Draw();
 	
-	glUseProgram(0);	modelMatrix = modelt;
-	textureMatrix = texturet;	glDisable(GL_DEPTH_TEST);
+	glUseProgram(0);
+
+	modelMatrix = modelt;
+	textureMatrix = texturet;
+
+	glDisable(GL_DEPTH_TEST);
 	glDisable(GL_BLEND);
-	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);}
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+}
